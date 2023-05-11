6a6a05b29f5357f2b8d82c171f15f96bb44467ad
Matthew Steinkamp <mdsteinkamp@gmail.com>
2023-05-10 19:32:44 -0700

    commit tree 70dbf292b9e01074a3c326b311eef16ddeb670e2

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,23 @@
-# Your code goes here!
+require "pry"
+
+
+class Anagram
+  attr_accessor :word
+
+  def initialize(word)
+    @word = word
+  end
+
+  def match(array)
+    # word_array = @word.chars.sort
+    matched_array = []
+    array.map do |word|
+      if word.chars.sort == @word.chars.sort
+        matched_array << word
+      end
+      # binding.pry
+    end
+    p matched_array
+  end
+
+end
